3992d0cbd0558b07affd91fc9643589d59140193
truizdiaz <tomiruizdiaz@gmail.com>
2020-10-23 17:16:28 -0300

    Random parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   dadoIzquierda = '../assets/img/dice1.png';
   dadoDerecha = '../assets/img/dice4.png';
-  numero1: number;
-  numero2: number;
+  numero1 = 1;
+  numero2 = 2;
 
   tirarDados(): void {
     this.numero1 = Math.round(Math.random() * 5) + 1;
     this.numero2 = Math.round(Math.random() * 5) + 1;
-    console.log(this.numero1);
-    console.log(this.numero2);
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
   }
 
   
